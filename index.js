09a2c4e8b9fb3b72089c812bde02edc663765fbd
Sameer12727 <sameergamer442@gmail.com>
2026-04-05 14:34:11 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+// index.js
+console.log("-----------------------------------------");
+console.log("SS ARENA SERVER TEST: SUCCESS!");
+console.log("Aapka Mobile Server sahi kaam kar raha hai.");
+console.log("Bot Name: Test-Worker-01");
+console.log("-----------------------------------------");
+
+// Ye line bot ko zinda rakhe gi taake PM2 ise stop na kare
+setInterval(() => {
+    console.log("Server Status: Online and Running...");
+}, 10000);
